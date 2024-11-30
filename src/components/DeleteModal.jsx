4abdd6da6f5daba7dba221aa3334cd740fa82539
betelhemalemayehu <Betelhem.L.A@gmail.com>
2024-11-29 20:32:19 -0500

    updated text and styling for delete modal

--- src/components/DeleteModal.jsx
+++ src/components/DeleteModal.jsx
@@ -9,19 +9,19 @@ const DeleteModal = ({ completedCount, onDelete, onClose }) => {
       aria-labelledby="delete-modal-title"
     >
       <div className="delete-modal">
-        <h1 id="delete-modal-title" className="delete-modal__title">
+        <h2 id="delete-modal-title" className="delete-modal__title">
           Are you sure?
-        </h1>
+        </h2>
         <p className="delete-modal__message">
-          Are you sure you want to delete your account forever?{" "}
+          Are you sure you want to delete your BodyLingo account forever?{" "}
         </p>
-        <h2>
+        <h3>
           {completedCount === 0
             ? "There's still time to learn!"
             : completedCount === 1
             ? "You will lose progress on 1 Pose!"
             : `You will lose progress on ${completedCount} poses!`}
-        </h2>
+        </h3>
         <img src="/assets/images/pincha_animation.png" alt="pincha site logo" />
         <div className="delete-modal__actions">
           <button className="button button--secondary" onClick={onClose}>
